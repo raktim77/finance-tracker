8badfa72e51bc695b5fb3ac109e3ef817a94455c
Raktim <rrouth77@gmail.com>
2025-08-22 00:39:06 +0530

    added git analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           {/* <Route path="/reports" element={<Reports />} /> */}
         </Routes>
       </MainLayout>
+      <Analytics />
     </Router>
   );
 }
